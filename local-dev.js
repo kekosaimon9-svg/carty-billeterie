2d24e65606bb2b3518b29593689a07473e181cc9
Yembit Saimon Pierre <digitechweb98@gmail.com>
2026-09-25 04:36:34 +0000

    Force Vercel static build

--- local-dev.js
+++
@@ -1,9 +0,0 @@
-const app = require('./api/index');
-const http = require('http');
-
-const server = http.createServer(app);
-const PORT = process.env.PORT || 8085;
-
-server.listen(PORT, () => {
-    console.log(`Serveur local (Supabase activé) démarré sur http://localhost:${PORT}`);
-});
